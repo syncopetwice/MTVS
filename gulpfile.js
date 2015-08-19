2ec5bce5cb218eb68b1008c5b0d9ef373c382e1f
Oleg Drozdov <oleg.drozdov.vrp@vrpinc.com>
2015-08-19 18:25:14 +0300

    Fixed UN-1, UN-42

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ var
 
 	gulp.task('copy', function(){
 		return gulp.src(staticResources, { base:'assets/'})
-			.pipe(gulp.dest('C:\\Maven\\Motivis Core\\resource-bundles\\assets.resource\\'));
+			.pipe(gulp.dest('C:\\Maven\\Motivis QA Dev\\resource-bundles\\assets.resource\\'));
 	});
 
 	gulp.task('watch', function() {
